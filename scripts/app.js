49e8335ef8811ceefe693c700e6177b1543b975e
quiz1991 <quizip@gmail.com>
2014-06-03 11:38:05 -0400

    live review-post form added, review controller created, angular validation and form style added

--- scripts/app.js
+++ scripts/app.js
@@ -15,8 +15,25 @@
 		this.isSelected = function(checkTab) {
 			return this.tab === checkTab;
 		};
-		
 	});
+
+	app.controller('GalleryController', function(){
+    this.current = 0;
+
+    this.setCurrent = function(newGallery){
+      this.current = newGallery || 0;
+    };
+  });
+
+  app.controller('ReviewController', function() {
+    this.review = {};
+
+    this.addReview = function(product) {
+    	this.review.createdOn = Date.now();
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+  });
 
 	var gems = [
     {
